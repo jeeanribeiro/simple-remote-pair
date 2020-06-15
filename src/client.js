244c30f72a7b15a36153cdae9e327c76dfd744d6
Jean Ribeiro <jeanmendesrib@gmail.com>
2020-06-15 19:25:42 -0300

    Adds key-event-plus and fixes indentation

--- src/client.js
+++ src/client.js
@@ -1,157 +1,154 @@
 var io = require('socket.io-client');
 var Peer = require('simple-peer');
 var wrtc = require('wrtc');
+var keyEventListener = require('key-event-plus');
 
 var socket = io(window.location.href);
 
 var sessionId;
 socket.on('connect', function() {
-    sessionId = socket.id;
+	sessionId = socket.id;
 });
 
 if (location.hash === '#host') {
 
-    socket.emit('newHost');
+	socket.emit('newHost');
 
-    var hostStream;
-    window.onload = function() {
-        var button = document.getElementById('displayToggle');
-        button.style.display = 'block';
+	var hostStream;
+	window.onload = function() {
+		var button = document.getElementById('displayToggle');
+		button.style.display = 'block';
 
-        var video = document.getElementById('display');
-        display.style.display = 'none';
+		var video = document.getElementById('display');
+		display.style.display = 'none';
 
-        button.addEventListener('click', function() {
-            navigator.mediaDevices.getDisplayMedia({ video: true })
-                .then(function(stream) {
-                    hostStream = stream;
-                })
-                .catch(function(error) {
-                    console.log('error', error);
-                })
-        })
-    }
+		button.addEventListener('click', function() {
+			navigator.mediaDevices.getDisplayMedia({ video: true })
+				.then(function(stream) {
+					hostStream = stream;
+				})
+				.catch(function(error) {
+					console.log('error', error);
+				})
+		})
+	}
 
-    var connectionList = [];
+	var connectionList = [];
 
-    socket.on('newConnection', function(socketId) {
-        var connection = {
-            socketId: socketId,
-            peer: new Peer({ initiator: true, trickle: false, wrtc: wrtc, stream: hostStream }),
-            offer: '',
-            answer: ''
-        }
+	socket.on('newConnection', function(socketId) {
+		var connection = {
+			socketId: socketId,
+			peer: new Peer({ initiator: true, trickle: false, wrtc: wrtc, stream: hostStream }),
+			offer: '',
+			answer: ''
+		}
 
-        connection.peer.on('signal', function(offer) {
-            connection.offer = JSON.stringify(offer);
-            socket.emit('newOffer', connection);
-        })
+		connection.peer.on('signal', function(offer) {
+			connection.offer = JSON.stringify(offer);
+			socket.emit('newOffer', connection);
+		})
 
-        socket.on('newAnswer', function(answer) {
-            if (answer.socketId === connection.socketId) {
-                connection.answer = answer;
-                connection.peer.signal(connection.answer.answer);
-            }
-        })
+		socket.on('newAnswer', function(answer) {
+			if (answer.socketId === connection.socketId) {
+				connection.answer = answer;
+				connection.peer.signal(connection.answer.answer);
+			}
+		})
 
-        connectionList.push(connection);
-    })
+		connectionList.push(connection);
+	})
 
-    connectionList.forEach(function (connection) {
-        connection.peer.on('data', function (data) {
-            console.log('data: ' + data);
-        })
+	connectionList.forEach(function (connection) {
+		connection.peer.on('data', function (data) {
+			console.log('data: ' + data);
+		})
 
-        connection.peer.on('error', function(error) {
-            console.log('error', error);
-        })
-    })
+		connection.peer.on('error', function(error) {
+			console.log('error', error);
+		})
+	})
 
 } else {
 
-    var peer = new Peer({ trickle: false, wrtc: wrtc });
+	var peer = new Peer({ trickle: false, wrtc: wrtc });
 
-    socket.on('newOffer', function(connection) {
-        console.log(connection);
-        peer.signal(connection.offer);
-    })
+	socket.on('newOffer', function(connection) {
+		console.log(connection);
+		peer.signal(connection.offer);
+	})
 
-    peer.on('signal', function(answer) {
-        answer = {
-            answer: JSON.stringify(answer),
-            socketId: sessionId
-        }
-        socket.emit('newAnswer', answer);
-    })
+	peer.on('signal', function(answer) {
+		answer = {
+			answer: JSON.stringify(answer),
+			socketId: sessionId
+		}
+		socket.emit('newAnswer', answer);
+	})
 
-    peer.on('stream', function(stream) {
-        var video = document.querySelector('video');
+	peer.on('stream', function(stream) {
+		var video = document.querySelector('video');
 
-        if ('srcObject' in video) {
-            video.srcObject = stream;
-        } else {
-            video.src = window.URL.createObjectURL(stream);
-        }
-    })
+		if ('srcObject' in video) {
+			video.srcObject = stream;
+		} else {
+			video.src = window.URL.createObjectURL(stream);
+		}
+	})
 
-    peer.on('connect', function() {
-        peer.send('peer connected');
-    })
+	peer.on('connect', function() {
+		peer.send('peer connected');
+	})
 
-    peer.on('error', function(error) {
-        console.log('error', error);
-    })
+	peer.on('error', function(error) {
+		console.log('error', error);
+	})
 
-    window.onload = function() {
-        var video = document.querySelector('video');
+	window.onload = function() {
+		var video = document.querySelector('video');
 
-        class coordsAndSize {
-            constructor(event, video) {
-                this.x = event.clientX - video.getBoundingClientRect().left;
-                this.y = event.clientY - video.getBoundingClientRect().top;
-                this.videoWidth = video.getBoundingClientRect().width;
-                this.videoHeight = video.getBoundingClientRect().height;
-            }
-        }
+		class coordsAndSize {
+			constructor(event, video) {
+				this.x = event.clientX - video.getBoundingClientRect().left;
+				this.y = event.clientY - video.getBoundingClientRect().top;
+				this.videoWidth = video.getBoundingClientRect().width;
+				this.videoHeight = video.getBoundingClientRect().height;
+			}
+		}
 
-        video.addEventListener('click', function (event) {
-            socket.emit('leftClick', new coordsAndSize(event, video));
-        })
+		video.addEventListener('click', function (event) {
+			socket.emit('leftClick', new coordsAndSize(event, video));
+		})
 
-        video.addEventListener('contextmenu', function (event) {
-            event.preventDefault();
-            socket.emit('rightClick', new coordsAndSize(event, video));
-        })
+		video.addEventListener('contextmenu', function (event) {
+			event.preventDefault();
+			socket.emit('rightClick', new coordsAndSize(event, video));
+		})
 
-        function emitDragMouse (event) {
-            socket.emit('dragMouse', new coordsAndSize(event, video));
-        }
+		function emitDragMouse (event) {
+			socket.emit('dragMouse', new coordsAndSize(event, video));
+		}
 
-        video.addEventListener('mousedown', function () {
-            socket.emit('mouseDown', new coordsAndSize(event, video));
-            video.addEventListener('mousemove', emitDragMouse);
-        })
+		video.addEventListener('mousedown', function () {
+			socket.emit('mouseDown', new coordsAndSize(event, video));
+			video.addEventListener('mousemove', emitDragMouse);
+		})
 
-        video.addEventListener('mouseup', function() {
-            socket.emit('mouseUp');
-            video.removeEventListener('mousemove', emitDragMouse);
-        })
+		video.addEventListener('mouseup', function() {
+			socket.emit('mouseUp');
+			video.removeEventListener('mousemove', emitDragMouse);
+		})
 
-        video.addEventListener('wheel', function(event) {
-            socket.emit('scroll', {
-                x: event.deltaX,
-                y: event.deltaY,
-            })
-        })
+		video.addEventListener('wheel', function(event) {
+			socket.emit('scroll', {
+				x: event.deltaX,
+				y: event.deltaY,
+			})
+		})
 
-        window.addEventListener('keydown', function (event) {
-            socket.emit('keyDown', event.key)
-        })
+		keyEventListener(20, function (event) {
+			socket.emit('keyEvent', event);
+		})
 
-        window.addEventListener('keyup', function (event) {
-            socket.emit('keyUp', event.key)
-        })
-
-    }
+	}
 
 }
--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.set('view engine', 'html');
 app.use(cors());
 
 app.use('/', (req, res) => {
-    res.render('index.html');
+	res.render('index.html');
 })
 
 let connectedUsers = [];
@@ -26,101 +26,89 @@ let hostId;
 
 io.on('connection', socket => {
 
-    const connectedUser = {
-        id: socket.id,
-        number: connectedUsers.length + 1,
-    }
-    connectedUsers.push(connectedUser);
-    console.log('User connection\n', connectedUsers);
+	const connectedUser = {
+		id: socket.id,
+		number: connectedUsers.length + 1,
+	}
+	connectedUsers.push(connectedUser);
+	console.log('User connection\n', connectedUsers);
 
-    socket.on('newHost', () => {
-        console.log('new host');
-        hostId = socket.id;
-    })
+	socket.on('newHost', () => {
+		console.log('new host');
+		hostId = socket.id;
+	})
 
-    io.to(hostId).emit('newConnection', socket.id);
+	io.to(hostId).emit('newConnection', socket.id);
 
-    socket.on('disconnect', () => {
-        i = 0;
-        connectedUsers = connectedUsers.filter(connectedUser => {
-            return connectedUser.id !== socket.id;
-        }).map(connectedUser => {
-            i++;
-            return {
-                id: connectedUser.id,
-                number: connectedUser.number = i,
-            }
-        })
-        console.log('User disconnection\n', connectedUsers);
-    })
+	socket.on('disconnect', () => {
+		i = 0;
+		connectedUsers = connectedUsers.filter(connectedUser => {
+			return connectedUser.id !== socket.id;
+		}).map(connectedUser => {
+			i++;
+			return {
+				id: connectedUser.id,
+				number: connectedUser.number = i,
+			}
+		})
+		console.log('User disconnection\n', connectedUsers);
+	})
 
-    socket.on('newOffer', data => {
-        io.to(data.socketId).emit('newOffer', data);
-    })
+	socket.on('newOffer', data => {
+		io.to(data.socketId).emit('newOffer', data);
+	})
 
-    socket.on('newAnswer', answer => {
-        io.to(hostId).emit('newAnswer', answer);
-    })
+	socket.on('newAnswer', answer => {
+		io.to(hostId).emit('newAnswer', answer);
+	})
 
-    function convertCoord(coords, xy) {
-        if (xy === 'x') {
-            return (robot.getScreenSize().width * coords.x) / coords.videoWidth;
-        } else if (xy === 'y') {
-            return (robot.getScreenSize().height * coords.y) / coords.videoHeight;
-        } else {
-            return;
-        }
-    }
+	function convertCoord(coords, xy) {
+		if (xy === 'x') {
+			return (robot.getScreenSize().width * coords.x) / coords.videoWidth;
+		} else if (xy === 'y') {
+			return (robot.getScreenSize().height * coords.y) / coords.videoHeight;
+		} else {
+			return;
+		}
+	}
 
-    socket.on('leftClick', coords => {
-        robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
-    })
+	socket.on('leftClick', coords => {
+		robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
+	})
 
-    socket.on('rightClick', coords => {
-        robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
-        robot.mouseClick('right');
-    })
+	socket.on('rightClick', coords => {
+		robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
+		robot.mouseClick('right');
+	})
 
-    socket.on('mouseDown', coords => {
-        robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
-        robot.mouseToggle('down');
-    })
+	socket.on('mouseDown', coords => {
+		robot.moveMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
+		robot.mouseToggle('down');
+	})
 
-    socket.on('dragMouse', coords => {
-        robot.dragMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
-    })
+	socket.on('dragMouse', coords => {
+		robot.dragMouse(convertCoord(coords, 'x'), convertCoord(coords, 'y'));
+	})
 
-    socket.on('mouseUp', () => {
-        robot.mouseToggle('up');
-    })
+	socket.on('mouseUp', () => {
+		robot.mouseToggle('up');
+	})
 
-    socket.on('scroll', delta => {
-        robot.scrollMouse(delta.x, delta.y);
-    })
+	socket.on('scroll', delta => {
+		robot.scrollMouse(delta.x, delta.y);
+	})
 
-    socket.on('keyDown', key => {
-        if (key.length !== 1 && key !== ' ') {
-            key = dict[key];
-        }
+	socket.on('keyEvent', event => {
+		if (event.key.length !== 1 && event.key !== ' ') {
+			event.key = dict[event.key];
+		}
 
-        try {
-            robot.keyToggle(key, 'down');
-        } catch (err) {
-            console.log('error', err);
-        }
-    })
-
-    socket.on('keyUp', key => {
-        if (key.length !== 1 && key !== ' ') {
-            key = dict[key];
-        }
-
-        try {
-            robot.keyToggle(key, 'up');
-        } catch (err) {
-            console.log('error', err);
-        }
-    })
+		try {
+			robot.keyToggle(event.key, event.state);
+		} catch (err) {
+			console.log('error', err);
+		}
+	})
 
 })
 
